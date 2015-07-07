7340d125866d8b34b0b245d557a77f2fd2ef9896
minisystem <jeffrey.lee@gmail.com>
2015-07-06 23:13:52 -0400

    removed calls to set_dac and pot_to_dac arrays used in scan_pots_and_update_control_voltages.c

--- monapollo-firmware/monapollo-firmware/scan_pots_and_update_control_voltages.c
+++ monapollo-firmware/monapollo-firmware/scan_pots_and_update_control_voltages.c
@@ -19,29 +19,6 @@ volatile uint16_t tune_offset = 0; //fine tune offset to display
 struct control_voltage vco1_pitch_cv = {VCO1_PITCH, DAC_MUX_EN1};
 struct control_voltage vco2_pitch_cv = {VCO2_PITCH, DAC_MUX_EN1};	
 
-//First group of pots inputs 0-15 on U2 demulitplexer
-volatile uint8_t dac_pot_decoder_0 [16][2] = { //[DAC MUX CHANNEL][DAC MUX ADDRESS]
-
-	{VCO2_MIX,		DAC_MUX_EN1}, 
-	{VCO1_MIX,		DAC_MUX_EN1},
-	{PITCH_EG2,		DAC_MUX_EN1},
-	{PITCH_VCO2,	DAC_MUX_EN0},
-	{PITCH_LFO,		DAC_MUX_EN0},
-	{PWM_LFO,		DAC_MUX_EN0},
-	{PWM_EG2,		DAC_MUX_EN0},
-	{VCO1_PW,		DAC_MUX_EN0},
-	{FINE,			DAC_MUX_EN1},
-	{TUNE,			DAC_MUX_EN0},
-	{LFO_RATE,		DAC_MUX_EN0},
-	{0,				0}, //ARP_RATE - pot is read by ADC but nothing is written to DAC os use dummy data for now
-	{GLIDE,			DAC_MUX_EN1},
-	{AMP_LFO,		DAC_MUX_EN2},
-	{VOLUME,		DAC_MUX_EN2},
-	{VCO2_PW,		DAC_MUX_EN1}															
-};
-
-
-
 struct control_voltage vco2_mix_cv		={VCO2_MIX,		DAC_MUX_EN1};
 struct control_voltage vco1_mix_cv		={VCO1_MIX,		DAC_MUX_EN1};
 struct control_voltage pitch_eg2_cv		={PITCH_EG2,	DAC_MUX_EN1};	
@@ -60,6 +37,7 @@ struct control_voltage vco2_pw_cv		={VCO2_PW,		DAC_MUX_EN1};
 
 struct control_voltage arp_rate_null	={0,0}; //null control voltage for arp rate pointer (only pot that does not does not have its value used to set a control voltage by the DAC)
 
+//First group of pots inputs 0-15 on U2 demulitplexer
 struct control_voltage *pot_decoder_0[16] = {
 	
 	&vco2_mix_cv,
@@ -81,25 +59,6 @@ struct control_voltage *pot_decoder_0[16] = {
 		
 };
 
-//Second group of pot inputs 1-15 (input 0 is grounded) on U4 demultiplexer
-volatile uint8_t dac_pot_decoder_1 [15][2] = {
-	
-	{FIL_EG2,		DAC_MUX_EN2},
-	{RES,			DAC_MUX_EN2},
-	{CUTOFF,		DAC_MUX_EN2},
-	{KEY_TRACK,		DAC_MUX_EN2},
-	{FIL_VCO2,		DAC_MUX_EN2},
-	{FIL_LFO,		DAC_MUX_EN2},
-	{NOISE_MIX,		DAC_MUX_EN0},				
-	{ATTACK_2,		DAC_MUX_EN3},
-	{ATTACK_1,		DAC_MUX_EN3},
-	{DECAY_2,		DAC_MUX_EN3},
-	{DECAY_1,		DAC_MUX_EN3},
-	{SUSTAIN_2,		DAC_MUX_EN3},
-	{SUSTAIN_1,		DAC_MUX_EN3},
-	{RELEASE_2,		DAC_MUX_EN3},
-	{RELEASE_1,		DAC_MUX_EN3}							
-};
 
 struct control_voltage fil_eg2_cv		={FIL_EG2,		DAC_MUX_EN2};
 struct control_voltage res_cv			={RES,			DAC_MUX_EN2};
@@ -116,7 +75,8 @@ struct control_voltage sustain_2_cv		={SUSTAIN_2,	DAC_MUX_EN3};
 struct control_voltage sustain_1_cv		={SUSTAIN_1,	DAC_MUX_EN3};
 struct control_voltage release_2_cv		={RELEASE_2,	DAC_MUX_EN3};
 struct control_voltage release_1_cv		={RELEASE_1,	DAC_MUX_EN3};						 							 			
-	
+
+//Second group of pot inputs 1-15 (input 0 is grounded) on U4 demultiplexer	
 struct control_voltage *pot_decoder_1[15] = {
 	
 	&fil_eg2_cv,
@@ -148,11 +108,9 @@ void scan_pots_and_update_control_voltages(void) {
 			uint16_t tune_value = 6303;//9759; //init CV offset of about -5.8V
 			if (i == 9) tune_value += 1638; //add an octave (1V) to VCO2 pitch
 			if (adc_value >= 512) {
-				//set_dac(dac_pot_decoder_0[i][1],dac_pot_decoder_0[i][0],(tune_value + (adc_value - 512)));
 				set_control_voltage(pot_decoder_0[i],(tune_value + (adc_value - 512)));
 				tune_offset = adc_value - 512;
 			} else {
-				//set_dac(dac_pot_decoder_0[i][1],dac_pot_decoder_0[i][0],(tune_value - (512- adc_value)));
 				set_control_voltage(pot_decoder_0[i],(tune_value - (512- adc_value)));
 				tune_offset = adc_value;
 			}
@@ -162,7 +120,6 @@ void scan_pots_and_update_control_voltages(void) {
 			//store ARP pot value, but don't set DAC
 			
 		} else {
-			//set_dac(dac_pot_decoder_0[i][1],dac_pot_decoder_0[i][0], adc_value << 4);
 			set_control_voltage(pot_decoder_0[i], adc_value << 4);
 		}
 		
@@ -172,25 +129,10 @@ void scan_pots_and_update_control_voltages(void) {
 	for (int i = 0; i <=14; i++) //first U4 input is grounded - only 15 pots, not 16 on second mux
 	{
 		
-		//DATA_BUS = i+1; //U4 input 0 is not used (grounded)
-		//POT_MUX &= ~(1<<POTMUX_EN1);
-		//_delay_us(2); //ADC settling time. Previously used 10 us, testing 2 us now.
-		//ADCSRA |= (1<<ADSC); //start ADC conversion
-		//while ((ADCSRA & (1<<ADSC))); //wait for ADC conversion to complete (13 cycles of ADC clock) - need to figure out what to do with this time - would interrupt be more efficient?
-		//POT_MUX |= (1<<POTMUX_EN1); //disable pot multiplexer U2
-		////adc_previous = adc_value;
-		//adc_value = ADCL;
-		//adc_value = adc_value | (ADCH <<8);
-		
 		adc_value = read_pot(POTMUX_EN1, i+1);
-		
-		//set_dac(dac_pot_decoder_1[i][1],dac_pot_decoder_1[i][0], adc_value << 4);
 		set_control_voltage(pot_decoder_1[i], adc_value <<4);
 
 	}
-	
-	//set_dac(DAC_MUX_EN1, VCO1_PITCH, 0); //OSCA PITCH CV
-	//set_dac(DAC_MUX_EN1, VCO2_PITCH, 0); //OSCB PITCH CV
 
 	set_control_voltage(&vco1_pitch_cv, 0);
 	set_control_voltage(&vco2_pitch_cv, 0);
